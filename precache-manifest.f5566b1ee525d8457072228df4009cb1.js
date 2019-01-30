3d128de985f465e21e559fbb5b8d74a68bc20b71
viniciusofp <viniciusofp@gmail.com>
2019-01-30 10:48:36 -0200

    public dir

---
+++ precache-manifest.f5566b1ee525d8457072228df4009cb1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "c76002989ad31cdc35e9",
+    "url": "/static/css/main.6f1fe496.chunk.css"
+  },
+  {
+    "revision": "c76002989ad31cdc35e9",
+    "url": "/static/js/main.c7600298.chunk.js"
+  },
+  {
+    "revision": "c83ff09cfd2746135d19",
+    "url": "/static/css/1.28293f03.chunk.css"
+  },
+  {
+    "revision": "c83ff09cfd2746135d19",
+    "url": "/static/js/1.c83ff09c.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "3cce106a1a0c37d5daf7f5613e530e92",
+    "url": "/index.html"
+  }
+];
