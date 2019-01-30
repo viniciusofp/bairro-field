57546c0ec7f0632d93f6be80ed53b97dd5ee4d87
viniciusofp <viniciusofp@gmail.com>
2019-01-30 10:56:07 -0200

    Updates

---
+++ precache-manifest.ee97b446cfd17aa7d790c7a40ddea67c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "7539c932e0213131ac42",
+    "url": "/bairro-field/static/css/main.6f1fe496.chunk.css"
+  },
+  {
+    "revision": "7539c932e0213131ac42",
+    "url": "/bairro-field/static/js/main.7539c932.chunk.js"
+  },
+  {
+    "revision": "c83ff09cfd2746135d19",
+    "url": "/bairro-field/static/css/1.28293f03.chunk.css"
+  },
+  {
+    "revision": "c83ff09cfd2746135d19",
+    "url": "/bairro-field/static/js/1.c83ff09c.chunk.js"
+  },
+  {
+    "revision": "b2f993ff254712f8fe69",
+    "url": "/bairro-field/static/js/runtime~main.b2f993ff.js"
+  },
+  {
+    "revision": "a3723b67a774579441ee6c1365d533a0",
+    "url": "/bairro-field/index.html"
+  }
+];
